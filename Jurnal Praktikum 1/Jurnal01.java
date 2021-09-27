4c770d7b8f1a5bd919aeb0b2cc35257f433331ba
nalda24 <rakrryaudi@gmail.com>
2021-09-27 19:54:16 +0700

    Jurnal01.java

---
+++ Jurnal Praktikum 1/Jurnal01.java
@@ -0,0 +1,8 @@
+public class Jurnal01 {
+	
+	public static void main(String[] args) {
+		System.out.println("Nama : Raka Ryaudi Nalda Nata Dikarta");
+		System.out.println("NIM : 6706210104");
+		System.out.println("Kelas : D3IF-4503");
+	}
+}
